8183df1e8fd3f7cf20cff47aedba24aa58836111
Matheus B. Sousa <matheusbsousa2@gmail.com>
2026-09-20 19:34:48 -0300

    Update GroupController.java

--- AI_FinanceApp/src/main/java/MatheusAPI/s/AI_FinanceApp/group/GroupController.java
+++ AI_FinanceApp/src/main/java/MatheusAPI/s/AI_FinanceApp/group/GroupController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class GroupController {
 
     private final GroupService groupService;
+    private final UserAccountService userAccountService;
 
     @PostMapping
     public ResponseEntity<Group> create(@RequestBody CreateGroupRequest request) {
