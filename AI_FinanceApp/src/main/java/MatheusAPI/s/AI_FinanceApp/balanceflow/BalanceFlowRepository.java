86c1d7a4d47be18366dc5766206ca0841bd63e23
Matheus B. Sousa <matheusbsousa2@gmail.com>
2026-09-20 19:49:11 -0300

    Update BalanceFlowRepository.java

--- AI_FinanceApp/src/main/java/MatheusAPI/s/AI_FinanceApp/balanceflow/BalanceFlowRepository.java
+++ AI_FinanceApp/src/main/java/MatheusAPI/s/AI_FinanceApp/balanceflow/BalanceFlowRepository.java
@@ -2,9 +2,11 @@ package MatheusAPI.s.AI_FinanceApp.balanceflow;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BalanceFlowRepository extends JpaRepository<BalanceFlow, Long> {
     List<BalanceFlow> findByGroupId(Long groupId);
     List<BalanceFlow> findByAccountId(Long accountId);
+    List<BalanceFlow> findByGroupIdAndCreateTimeGreaterThanEqual(Long groupId, LocalDateTime start);
 }
