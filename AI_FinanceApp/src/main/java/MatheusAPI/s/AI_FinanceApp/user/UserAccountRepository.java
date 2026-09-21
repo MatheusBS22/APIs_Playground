9a236eae3f62ad2eede5d1be0156dd7fef5d7b19
Matheus B. Sousa <matheusbsousa2@gmail.com>
2026-09-21 13:07:28 -0300

    Update UserAccountRepository.java

--- AI_FinanceApp/src/main/java/MatheusAPI/s/AI_FinanceApp/user/UserAccountRepository.java
+++ AI_FinanceApp/src/main/java/MatheusAPI/s/AI_FinanceApp/user/UserAccountRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     List<UserAccount> findByGroupId(Long groupId);
     Optional<UserAccount> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
